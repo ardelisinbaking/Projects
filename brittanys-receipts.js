71f5c4f2ee1d038962334ec9c857a82d5e35d571
Ardelis Yeung <oyu.ardelis@gmail.com>
2022-08-14 15:00:41 -0700

    Uses data set and renames functions
    A single function pulls data from a structured data set rather than multiple if/else functions for provincial data. Renames functions as verbs rather than object names. Does not employ console error.

--- brittanys-receipts.js
+++ brittanys-receipts.js
@@ -1,83 +1,39 @@
-  const brittanysPanties = (price) => {
-    let percentage = 12;
-    let tax = price * (percentage / 100);
-    let total = price + tax;
+  const province = {
+    bc: {
+      tax: 12,
+      location: "BC",
+    },
+    quebec: {
+      tax: 14.975,
+      location: "Quebec",
+    },
+    ontario: {
+      tax: 13,
+      location: "Ontario",
+    },
+  };
+  const taxCalculator = function (price, province) {
+    let actualTax = price * (province.tax / 100);
+    let total = price + actualTax;
     console.log(`
-    Brittany's Receipt (CAD)
-    price: $${price}
-    percentage: ${percentage}%
-    tax: $${tax.toFixed(2)}
-    total: $${total.toFixed(2)}
+    Brittany's Interprovincial Receipt
+    Price: $${price}
+    Province: ${province.location}
+    Tax %: ${province.tax}%
+    Tax: $${actualTax.toFixed(2)}
+    Total: $${total.toFixed(2)}
     `);
   };
-  brittanysPanties(33.99);
-  
-  const brittanysInterprovincialPanties = function (price, province) {
-    if (province == "BC") {
-      var percentage = 12;
-      let tax = price * (percentage / 100);
-      let total = price + tax;
-      console.log(`
-      Brittany's BC Panties Receipt
-      Price: $: ${price}
-      Province: : ${province}
-      Tax %: : ${percentage}%
-      Tax: : $${tax.toFixed(2)}
-      Total: : ${total.toFixed(2)}
-      `);
-    } else if (province == "Quebec") {
-      var percentage = 14.975;
-      let tax = price * (percentage / 100);
-      let total = price + tax;
-      console.log(`
-      Brittany's Quebec Panties Receipt
-      Price: $: ${price}
-      Province: : ${province}
-      Tax %: : ${percentage}%
-      Tax: : $${tax.toFixed(2)}
-      Total: : ${total.toFixed(2)}
-      `);
-    } else if (province == "Ontario") {
-      var percentage = 13;
-      let tax = price * (percentage / 100);
-      let total = price + tax;
-      console.log(`
-      Brittany's Ontario Panties Receipt
-      Price: $: ${price}
-      Province: : ${province}
-      Tax %: : ${percentage}%
-      Tax: : $${tax.toFixed(2)}
-      Total: : ${total.toFixed(2)}
-      `);
-    } else {
-      console.error("Brittany does not shop there.");
-    }
-  };
-  
-  brittanysInterprovincialPanties(32.99, "BC");
-  brittanysInterprovincialPanties(32.99, "Quebec");
-  brittanysInterprovincialPanties(32.99, "Ontario");
-  brittanysInterprovincialPanties(32.99, "Alberta");
+  taxCalculator(42.98, province.quebec);
   
   /*
-  const brittanysInterprovincialPanties = (price, province) => {
-    if (province === "BC") {
-      var percentage = 12;
-    }
-    if (province === "Quebec") {
-      var percentage = 14.975;
-    }
-    if (province === "Ontario") {
-      var percentage = 13;
-    }
-    let tax = price * (percentage / 100);
-    let total = price + tax;
-    console.log(`
-    Brittany's Interprovincial Panties Receipt
-    Price: $: ${price}
-    Province: : ${province}
-    Tax %: : ${percentage}%
-    Tax: : $${tax}
-    Total: : ${total.toFixed(2)}
-    `);
-  };/*
+  const province = {
+    bc: 12,
+    quebec: 14.975,
+    ontario: 13,
+    provinceIndicator: (bc, quebec, ontario) {
+      this.bc = bc;
+      this.quebec = quebec;
+      this.ontario = ontario;
+    },
+  };*/
